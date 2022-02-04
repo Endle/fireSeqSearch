1bde7abddc2011a339c2334f68731e711bc59018
Zhenbo Li <lizhenbo@yahoo.com>
2022-02-04 23:53:40 +0800

    Add a server script
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

--- run.sh
+++
@@ -1,2 +0,0 @@
- cd fireSeqSearch_addon
- web-ext run --firefox=/home/lizhenbo/bin/firefox/firefox-bin --verbose
---
+++ run_server.sh
@@ -0,0 +1,4 @@
+cd fire_seq_search_server
+cargo build
+cp --force target/debug/fire_seq_search_server .
+./fire_seq_search_server
