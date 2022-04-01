c9bcef217dfdb60ef04b17732f55b048a409c8e2
Zhenbo Li <lizhenbo@yahoo.com>
2022-03-31 20:00:38 -0400

    Edit for windows

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 cd fire_seq_search_server
 cargo build
-cp --force target/debug/fire_seq_search_server .
-./fire_seq_search_server --notebook-path /home/lizhenbo/src/logseq_notebook
+cp --force target/debug/fire_seq_search_server.exe ./fire_seq_search_server
+./fire_seq_search_server --notebook-path /c/Users/z2369li/Nextcloud/logseq_notebook
