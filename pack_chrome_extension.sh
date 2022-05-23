8937af6e82aca998178856baee8d8fd9ce3e8dc7
Zhenbo Li <lizhenbo@yahoo.com>
2022-05-23 19:02:43 -0400

    Still working on Chrome extension
    I'm hitting CORS error. No idea how to fix it. I have been working for days to find an example, but these extensions are all using manifest V2

--- pack_chrome_extension.sh
+++ pack_chrome_extension.sh
@@ -1,8 +1,8 @@
+rm -rf chrome_tmp
 cp -r fireSeqSearch_addon chrome_tmp
 cd chrome_tmp
 rm manifest.json
 mv manifest_chrome.json manifest.json
 zip -r -FS ../fireSeqSearch_chrome.zip * --exclude '*.git*'
 cd ..
-rm -rf chrome_tmp
 cp -f fireSeqSearch.zip /dev/shm
