55ed208c182f08e01c1bafd2bb7ee7328a6137d2
Zhenbo Li <lizhenbo@yahoo.com>
2022-05-30 11:01:18 -0400

    Handle directories in notebook
    https://github.com/Endle/fireSeqSearch/issues/23

--- fire_seq_search_server/src/main.rs
+++ fire_seq_search_server/src/main.rs
@@ -9,7 +9,7 @@ use std::fs;
 use serde_json;
 use serde::Serialize;
 
-use log::{info,debug};
+use log::{info,debug,warn,error};
 // use log::LevelFilter;
 use clap::{Command,arg};
 
@@ -140,24 +140,53 @@ fn indexing_documents(path: &str) -> tantivy::Index {
 
 
     for note in notebooks {
-        let note:std::fs::DirEntry = note.unwrap();
-        let note = note.path();
-        // println!("{:?}", &note);
-        let note_title = note.file_stem().unwrap().to_str().unwrap();
-        debug!("note title: {}", &note_title);
+        let note : std::fs::DirEntry = note.unwrap();
+        let note_option = read_md_file(note);
 
-        let contents :String = fs::read_to_string(&note)
-            .expect("Something went wrong reading the file");
-        debug!("Length: {}", contents.len());
+        match note_option {
+            Some((note_title, contents)) => {
+                debug!("Length: {}", contents.len());
 
-        let mut doc = Document::default();
-        doc.add_text(title, note_title);
-        doc.add_text(body, contents);
-        index_writer.add_document(doc);
+                let mut doc = Document::default();
+                doc.add_text(title, note_title);
+                doc.add_text(body, contents);
+                index_writer.add_document(doc);
+            },
+            None => ()
+        };
     }
 
     index_writer.commit().unwrap();
     index
+}
+
+fn read_md_file(note: std::fs::DirEntry) -> Option<(String, String)> {
+
+    if let Ok(file_type) = note.file_type() {
+        // Now let's show our entry's file type!
+        debug!("{:?}: {:?}", note.path(), file_type);
+        if file_type.is_dir() {
+            debug!("{:?} is a directory, skipping", note.path());
+            return None;
+        }
+    } else {
+        warn!("Couldn't get file type for {:?}", note.path());
+        return None;
+    }
+
+    let note_path = note.path();
+    let note_title = match note_path.file_stem() {
+        Some(osstr) => osstr.to_str().unwrap(),
+        None => {
+            error!("Couldn't get file_stem for {:?}", note.path());
+            return None;
+        }
+    };
+    debug!("note title: {}", &note_title);
+
+    let contents : String = fs::read_to_string(&note_path)
+        .expect("Something went wrong reading the file");
+    Some((note_title.to_string(),contents))
 }
 
 
