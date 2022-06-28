d49c50d4f0aee4b2235946362bc7be52fa879358
Zhenbo Li <lizhenbo@yahoo.com>
2022-06-28 17:19:14 -0400

    Add some basic support

--- fire_seq_search_server/src/main.rs
+++ fire_seq_search_server/src/main.rs
@@ -2,8 +2,9 @@ use warp::Filter;
 
 
 use tantivy::schema::*;
-use tantivy::Index;
+use tantivy::{Index,doc};
 use tantivy::ReloadPolicy;
+use cang_jie::CangJieTokenizer;
 
 use std::fs;
 use serde_json;
@@ -110,7 +111,7 @@ fn query(term: String, reader: &tantivy::IndexReader, query_parser: &tantivy::qu
     let query = query_parser.parse_query(&term).unwrap();
     let top_docs = searcher.search(&query, &tantivy::collector::TopDocs::with_limit(10))
         .unwrap();
-    let (schema, _title, _body) = build_schema_dev();
+    let (schema, _title, _body, _tokenizer) = build_schema_dev();
     let mut result = Vec::new();
     for (_score, doc_address) in top_docs {
         // _score = 1;
@@ -133,7 +134,7 @@ fn build_reader_parser(index: &tantivy::Index) -> (tantivy::IndexReader, tantivy
         .reader_builder()
         .reload_policy(ReloadPolicy::OnCommit)
         .try_into().unwrap();
-    let (_schema, title,body) = build_schema_dev();
+    let (_schema, title,body, _tokenizer) = build_schema_dev();
     let query_parser = tantivy::query::QueryParser::for_index(index, vec![title, body]);
     (reader, query_parser)
 }
@@ -143,8 +144,30 @@ fn indexing_documents(path: &str) -> tantivy::Index {
 
     // let index_path = TempDir::new().unwrap();
     // info!("Using temporary directory {:?}", index_path);
-    let (schema, title,body) = build_schema_dev();
+
+    let mut schema_builder = SchemaBuilder::default();
+    let text_indexing = TextFieldIndexing::default()
+        .set_tokenizer(cang_jie::CANG_JIE) // Set custom tokenizer
+        .set_index_option(IndexRecordOption::WithFreqsAndPositions);
+    let text_options = TextOptions::default()
+        .set_indexing_options(text_indexing)
+        .set_stored();
+    let tokenizer:CangJieTokenizer = CangJieTokenizer {
+        worker: std::sync::Arc::new(jieba_rs::Jieba::empty()), // empty dictionary
+        option: cang_jie::TokenizerOption::Unicode,
+    };
+
+
+    // let (schema, title,body, tokenizer) = build_schema_dev();
+
+
+    let title = schema_builder.add_text_field("title", text_options.clone());
+    let body = schema_builder.add_text_field("body", text_options);
+
+    let schema = schema_builder.build();
+
     let index = Index::create_in_ram(schema.clone());
+    index.tokenizers().register(cang_jie::CANG_JIE, tokenizer);
 
     let mut index_writer = index.writer(50_000_000).unwrap();
 
@@ -163,9 +186,11 @@ fn indexing_documents(path: &str) -> tantivy::Index {
                 debug!("Length: {}", contents.len());
 
                 let mut doc = Document::default();
-                doc.add_text(title, note_title);
-                doc.add_text(body, contents);
-                index_writer.add_document(doc);
+                // doc.add_text(title, note_title);
+                // doc.add_text(body, contents);
+                index_writer.add_document(
+                    doc!{ title => note_title, body => contents}
+                );
             },
             None => ()
         };
@@ -215,13 +240,27 @@ fn read_md_file(note: std::fs::DirEntry) -> Option<(String, String)> {
 }
 
 
-fn build_schema_dev() -> (tantivy::schema::Schema, tantivy::schema::Field, tantivy::schema::Field) {
+fn build_schema_dev() -> (tantivy::schema::Schema,
+                          tantivy::schema::Field,
+                          tantivy::schema::Field,
+                          CangJieTokenizer) {
     // TODO currently for dev, a bit hacky
-    let mut schema_builder = Schema::builder();
+    // let mut schema_builder = Schema::builder();
+    let mut schema_builder = SchemaBuilder::default();
+    let text_indexing = TextFieldIndexing::default()
+        .set_tokenizer(cang_jie::CANG_JIE) // Set custom tokenizer
+        .set_index_option(IndexRecordOption::WithFreqsAndPositions);
+    let text_options = TextOptions::default()
+        .set_indexing_options(text_indexing)
+        .set_stored();
+    let tokenizer:CangJieTokenizer = CangJieTokenizer {
+        worker: std::sync::Arc::new(jieba_rs::Jieba::empty()), // empty dictionary
+        option: cang_jie::TokenizerOption::Unicode,
+    };
     schema_builder.add_text_field("title", TEXT | STORED);
     schema_builder.add_text_field("body", TEXT);
     let schema = schema_builder.build();
     let title = schema.get_field("title").unwrap();
     let body = schema.get_field("body").unwrap();
-    (schema, title, body)
+    (schema, title, body, tokenizer)
 }
