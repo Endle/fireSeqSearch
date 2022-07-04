ec32e8d3ca1d4bc0ed5bc288fb7835b6c1f74586
Zhenbo Li <lizhenbo@yahoo.com>
2022-07-04 14:21:27 -0400

    Test passes

--- fire_seq_search_server/src/lib.rs
+++ fire_seq_search_server/src/lib.rs
@@ -64,3 +64,66 @@ impl Tokenizer for JiebaTokenizer {
         BoxTokenStream::from(JiebaTokenStream { tokens, index: 0 })
     }
 }
+
+
+
+#[cfg(test)]
+mod test_tokenizer {
+    #[test]
+    fn it_works() {
+        use tantivy::tokenizer::*;
+        let tokenizer = crate::JiebaTokenizer {};
+        let mut token_stream = tokenizer.token_stream(
+            "张华考上了北京大学；李萍进了中等技术学校；我在百货公司当售货员：我们都有光明的前途",
+        );
+        let mut tokens = Vec::new();
+        let mut token_text = Vec::new();
+        while let Some(token) = token_stream.next() {
+            tokens.push(token.clone());
+            token_text.push(token.text.clone());
+        }
+        // offset should be byte-indexed
+        assert_eq!(tokens[0].offset_from, 0);
+        assert_eq!(tokens[0].offset_to, "张华".bytes().len());
+        assert_eq!(tokens[1].offset_from, "张华".bytes().len());
+        // check tokenized text
+        assert_eq!(
+            token_text,
+            vec![
+                // "a",
+                "张华",
+                "考上",
+                "了",
+                "北京",
+                "大学",
+                "北京大学",
+                "；",
+                "李萍",
+                "进",
+                "了",
+                "中等",
+                "技术",
+                "术学",
+                "学校",
+                "技术学校",
+                "；",
+                "我",
+                "在",
+                "百货",
+                "公司",
+                "百货公司",
+                "当",
+                "售货",
+                "货员",
+                "售货员",
+                "：",
+                "我们",
+                "都",
+                "有",
+                "光明",
+                "的",
+                "前途"
+            ]
+        );
+    }
+}
