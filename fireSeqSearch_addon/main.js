3e33cec9287a454904ba51c861c4341fa140e3b3
Zhenbo Li <3221521+Endle@users.noreply.github.com>
2022-08-27 18:25:24 -0400

    Refactor js code, allowing layout change (#31)
    * Use lots of Promise and fetch()
    
    * Insert dom at last step
    
    * Benefits https://github.com/Endle/fireSeqSearch/issues/21

--- fireSeqSearch_addon/main.js
+++ fireSeqSearch_addon/main.js
@@ -1,6 +1,9 @@
 // MIT License
 // Copyright (c) 2021-2022 Zhenbo Li
 
+const fireSeqSearchDomId = "fireSeqSearchDom";
+
+
 function createElementWithText(type, text) {
     let x = document.createElement(type);
     x.textContent = text;
@@ -11,8 +14,8 @@ function wrapRawRecordIntoElement(rawRecord, serverInfo) {
     // rawRecord is String   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
 
     const name = serverInfo.notebook_name;
-    console.log("wrapping " + String(rawRecord) + " to notebook " + name);
-    console.log(typeof rawRecord);
+    //console.log("wrapping " + String(rawRecord) + " to notebook " + name);
+    //console.log(typeof rawRecord);
 
     const record = JSON.parse(rawRecord);
     console.log(typeof record);
@@ -33,129 +36,146 @@ function wrapRawRecordIntoElement(rawRecord, serverInfo) {
     return li;
 }
 
-function performSearchAgainstLogseq(keywords, serverInfo) {
-    const search_url = "http://127.0.0.1:3030/query/" + keywords;
 
-    function uglyExtraLine() {
-        let x = createElementWithText("br", "");
-        return x;
-    }
-    console.log(search_url);
-
-    function appendResultToSearchResult(rawSearchResult, dom) {
-
-
-        const count = rawSearchResult.length;
-
-        let hitCount = createElementWithText("div",
-            "We found " + count.toString() + " results in your logseq notebook");
-        hitCount.style.fontSize = "large";
-        dom.appendChild(hitCount);
-        dom.appendChild(uglyExtraLine());
-
-        let hitList = document.createElement("ul");
-        for (let rawRecord of rawSearchResult) {
-            // const e = document.createTextNode(record);
-            let e = wrapRawRecordIntoElement(rawRecord, serverInfo);
-            // e.style.
-            hitList.appendChild(e);
-            // console.log("Added an element to the list");
-        }
-        hitList.style.lineHeight = "150%";
-        dom.appendChild(hitList);
-    }
-
-    const fireSeqSearchDomId = "fireSeqSearchDom";
-
-    function getFireSeqDomToWebpage() {
-        function insertFireSeqDomToWebpage() {
-            let div = document.createElement("div");
-            div.appendChild(createElementWithText("p", "fireSeqSearch launched!"));
-            div.setAttribute("id", fireSeqSearchDomId);
-
-            document.body.insertBefore(div, document.body.firstChild);
-            console.log("inserted");
-            // Very hacky for google
-            if (window.location.toString().includes("google")) {
-                for (let i=0; i<6; ++i) {
-                    div.appendChild(uglyExtraLine());
-                }
-            }
-
-            return div;
-        }
-        let fireDom = document.getElementById(fireSeqSearchDomId);
-        if (fireDom === null) {
-            fireDom = insertFireSeqDomToWebpage();
-        }
-        return fireDom;
-    }
-
-    let fireSeqDom = getFireSeqDomToWebpage();
-    window.fetch(search_url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            appendResultToSearchResult(data, fireSeqDom)
-        });
-
-
+function uglyExtraLine() {
+    return createElementWithText("br", "");
 }
+
+// deprecated
+/*
+function getFireSeqDomOnWebpage() {
+
+    function insertFireSeqDomToWebpage() {
+        let div = document.createElement("div");
+        div.appendChild(createElementWithText("p", "fireSeqSearch launched!"));
+        div.setAttribute("id", fireSeqSearchDomId);
+
+        document.body.insertBefore(div, document.body.firstChild);
+        console.log("inserted");
+        // Very hacky for google
+        if (window.location.toString().includes("google")) {
+            for (let i=0; i<6; ++i) {
+                div.appendChild(uglyExtraLine());
+            }
+        }
+
+        return div;
+    }
+    let fireDom = document.getElementById(fireSeqSearchDomId);
+    if (fireDom === null) {
+        fireDom = insertFireSeqDomToWebpage();
+    }
+    return fireDom;
+}
+
+ */
 
 
 function checkUserOptions() {
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
+    return Promise.all([
+        /*global browser */
+        browser.storage.sync.get("debugStr"),
+        browser.storage.sync.get("ExperimentalLayout")
+    ]).then(function(res) {
+        console.log(res);
 
-    function onGot(item) {
-        console.log(item);
-    }
+        const options = {
+            debugStr: res[0].debugStr,
+            ExperimentalLayout: res[1].ExperimentalLayout,
+        }
+        console.log(options);
+        return options;
+    });
 
-    const getting = browser.storage.sync.get("debugStr");
-    getting.then(onGot, onError);
+    /*  .catch(function (error) {
+        console.log(error);
+    });
 
-    const expLay = browser.storage.sync.get("ExperimentalLayout");
-    expLay.then(onGot, onError);
+     */
 }
 
-(function() {
-    checkUserOptions();
+async function appendResultToSearchResult(fetchResultArray) {
+    const serverInfo = fetchResultArray[0];
+    const rawSearchResult = fetchResultArray[1];
+    const firefoxExtensionUserOption = await checkUserOptions();
+    const count = rawSearchResult.length;
 
-    function getSearchParameterFromCurrentPage() {
-        let searchParam;
+    console.log(firefoxExtensionUserOption);
 
-        function getSearchParameterOfSearx() {
-            let inputBox = document.getElementById("q");
-            // console.log(inputBox);
-            return inputBox.value;
+    let hitCount = createElementWithText("div",
+        "We found " + count.toString() + " results in your logseq notebook");
+    hitCount.style.fontSize = "large";
+
+    function createFireSeqDom() {
+        let div = document.createElement("div");
+        div.appendChild(createElementWithText("p", "fireSeqSearch launched!"));
+        div.setAttribute("id", fireSeqSearchDomId);
+
+        // document.body.insertBefore(div, document.body.firstChild);
+        // console.log("inserted");
+        // Very hacky for google
+        if (window.location.toString().includes("google")) {
+            for (let i=0; i<6; ++i) {
+                div.appendChild(uglyExtraLine());
+            }
         }
-
-        if (window.location.toString().includes("searx")) {
-            searchParam = getSearchParameterOfSearx();
-        } else {
-            // https://stackoverflow.com/a/901144/1166518
-            const urlParams = new URLSearchParams(window.location.search);
-            // console.log(urlParams);
-            searchParam = urlParams.get('q');
-        }
-
-        console.log("Got search param: ");
-        console.log(searchParam);
-        return searchParam;
+        return div;
     }
 
+    let dom = createFireSeqDom();
+    dom.appendChild(hitCount);
+    dom.appendChild(uglyExtraLine());
 
+    let hitList = document.createElement("ul");
+    for (let rawRecord of rawSearchResult) {
+        // const e = document.createTextNode(record);
+        let e = wrapRawRecordIntoElement(rawRecord, serverInfo);
+        // e.style.
+        hitList.appendChild(e);
+        // console.log("Added an element to the list");
+    }
+    hitList.style.lineHeight = "150%";
+    dom.appendChild(hitList);
+
+    document.body.insertBefore(dom, document.body.firstChild);
+}
+
+function getSearchParameterFromCurrentPage() {
+    let searchParam;
+
+    function getSearchParameterOfSearx() {
+        const inputBox = document.getElementById("q");
+        return inputBox.value;
+    }
+
+    if (window.location.toString().includes("searx")) {
+        searchParam = getSearchParameterOfSearx();
+    } else {
+        // https://stackoverflow.com/a/901144/1166518
+        const urlParams = new URLSearchParams(window.location.search);
+        // console.log(urlParams);
+        searchParam = urlParams.get('q');
+    }
+
+    console.log("Got search param: " + searchParam);
+    return searchParam;
+}
+
+
+(function() {
     const searchParameter = getSearchParameterFromCurrentPage();
 
-
-    window.fetch("http://127.0.0.1:3030/server_info")
-        // .then(response => console.log(response));
-        .then(response => response.json())
-        .then(serverInfo => {
-            console.log(serverInfo);
-            performSearchAgainstLogseq(searchParameter, serverInfo);
-        });
-
+    //https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/
+    Promise.all([
+        fetch("http://127.0.0.1:3030/server_info"),
+        fetch("http://127.0.0.1:3030/query/" + searchParameter)
+    ]).then(function (responses) {
+        return Promise.all(responses.map(function (response) {return response.json();}));
+    }).then(function (data) {
+        console.log(data);
+        appendResultToSearchResult(data);
+    }).catch(function (error) {
+        console.log(error);
+    });
 
 })();
