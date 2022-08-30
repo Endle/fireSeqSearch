6155bb2806170b7e2dfb767bd356c4e247b9cf8a
Zhenbo Li <lizhenbo@yahoo.com>
2022-08-30 19:31:56 -0400

    test_split_to_block

--- fire_seq_search_server/src/post_query/mod.rs
+++ fire_seq_search_server/src/post_query/mod.rs
@@ -1,4 +1,18 @@
 pub fn highlight_keywords_in_body(body: &str, term_tokens: &Vec<String>) -> String {
-
+    let blocks = split_body_to_blocks(body);
     String::from(body)
+}
+
+// TODO: current implementation is too naive, I believe it is buggy
+pub fn split_body_to_blocks(body: &str) -> Vec<String> {
+    let mut result = Vec::new();
+    for line in body.lines() {
+        // let t = line.trim();
+        let t = line.trim_start_matches(&['-', ' ']);
+        // println!("trim: {}", t);
+        if !t.is_empty() {
+            result.push(String::from(t));
+        }
+    }
+    result
 }
--- fire_seq_search_server/tests/unit_test_post_query.rs
+++ fire_seq_search_server/tests/unit_test_post_query.rs
@@ -1,6 +1,10 @@
 
-use fire_seq_search_server::post_query::highlight_keywords_in_body;
+use fire_seq_search_server::post_query::{highlight_keywords_in_body, split_body_to_blocks};
 
+fn get_english_text() -> String {
+    std::fs::read_to_string("tests/resource/pages/International Language, Past, Present & Future by Walter John Clark.md")
+        .expect("Should have been able to read the file")
+}
 
 #[test]
 fn test_empty_key() {
@@ -11,4 +15,16 @@ fn test_empty_key() {
     assert_eq!(4,4);
 
     //assert_eq!(&json, "{\"title\":\"Hello\"}");
+}
+
+
+#[test]
+fn test_split_to_block() {
+    // This part is still hacky
+    let contents = get_english_text();
+    let blocks = split_body_to_blocks(&contents);
+
+    assert_eq!("As an ounce of personal experience is worth a pound of second-hand recital, a brief statement may here be given of the way in which the present writer came to take up Esperanto, and of the experiences which soon led him to the conviction of its absolute practicability and utility.", &blocks[0]);
+    assert_eq!("Now, quite apart from the obvious fact that the nations will never agree to give the preference to the language of one of them to the prejudice of the others, this argument involves the 16 suggestion that an artificial language is no easier to learn than a natural one. We thus come to the question of ease as a qualification.", &blocks[12]);
+    assert_eq!(14, blocks.len());
 }
