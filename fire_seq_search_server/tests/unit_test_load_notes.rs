31264b1cb0d432ab2915ba62cd84d210d8d85ea8
Zhenbo Li <lizhenbo@yahoo.com>
2022-10-23 19:48:47 -0400

    fix test

--- fire_seq_search_server/tests/unit_test_load_notes.rs
+++ fire_seq_search_server/tests/unit_test_load_notes.rs
@@ -3,7 +3,7 @@ use fire_seq_search_server::load_notes::read_specific_path;
 #[test]
 fn load_articles() {
     let r = read_specific_path("tests/resource/pages");
-    assert_eq!(r.len(), 5);
+    assert_eq!(r.len(), 6);
     for (title,body) in &r{
         assert!(title.len()>0);
         assert!(body.len()>0);
