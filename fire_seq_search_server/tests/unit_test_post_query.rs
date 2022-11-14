bf05ab8733155c2fcd6d1dd6d49b5bb854f54444
Zhenbo Li <lizhenbo@yahoo.com>
2022-11-13 20:30:49 -0500

    remove wrong test cases

--- fire_seq_search_server/tests/unit_test_post_query.rs
+++ fire_seq_search_server/tests/unit_test_post_query.rs
@@ -1,4 +1,3 @@
-use tantivy::HasLen;
 use fire_seq_search_server::post_query::{highlight_keywords_in_body, split_body_to_blocks, split_by_single_token};
 
 fn get_english_text() -> String {
@@ -17,20 +16,14 @@ fn test_empty_key() {
     assert_eq!(&r, "");
 }
 
-#[test]
-fn test_highlight() {
-    let contents = get_english_text();
-    let v = vec![String::from("juxtaposition"), String::from("of"), String::from("pronunciation")];
-    let r = highlight_keywords_in_body(&contents, &v, 120);
-    assert_eq!(&r, "This was fairly convincing, but still having doubts on the question of <span class=\"fireSeqSearchHighlight\">pronunciation</span>, the writer resolved to attend the Esperanto Congress to be held at Geneva in August 1906. To 14 this end he continued to read Esperanto at odd minutes and took in an Esperanto gazette. About three weeks before the congress he got a member of his family to read aloud to him every day as far as possible a page or two of Esperanto, in order to attune his ear. He never had an opportunity of speaking the language before the congress, except once for a few minutes, when he travelled some distance to attend a meeting of the nearest English group. With all these people it was perfectly easy to converse in the common tongue, <span class=\"fireSeqSearchHighlight\">pronunciation</span> and national idiom being no bar in practice. In the face of these facts it is idle to oppose a universal artificial language on the score of impossibility or inadequacy. The theoretical <span class=\"fireSeqSearchHighlight\">pronunciation</span> difficulty completely crumbled away before the test of practice. The \"war-at-any-price party,\" the whole-hoggers à tous crins (the <span class=\"fireSeqSearchHighlight\">juxtaposition</span> of the two national idioms lends a certain realism, and heightens the effect of each), are therefore driven back on their second line of attack, if the Hibernianism may be excused. \"Yes,\" they say, \"your language may be possible, but, after all, why not learn an existing language, if you've got to learn one anyway?\"");
-}
+
 
 #[test]
 fn test_highlight_wrap() {
     let contents = "使用 git shallow clone 下载并编译 Thunderbird".to_string();
     let v = vec![String::from("thunderbird")];
     let r = highlight_keywords_in_body(&contents, &v, 120);
-    assert_eq!(&r, "使用 git shallow clone 下载并编译 <span class=\"fireSeqSearchHighlight\">thunderbird</span>");
+    assert_eq!(&r, "使用 git shallow clone 下载并编译 <span class=\"fireSeqSearchHighlight\">Thunderbird</span>");
 }
 
 
@@ -86,11 +79,3 @@ fn test_split_by_single_token_cyrillic() {
 }
 
 
-
-fn gen(s: Vec<&str>) -> Vec<String> {
-    let mut r = Vec::with_capacity(s.len());
-    for i in s {
-        r.push(String::from(i));
-    }
-    r
-}
