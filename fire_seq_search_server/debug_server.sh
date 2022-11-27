95d356dba2853035711e57cd29ff8fe084e4df17
Zhenbo Li <lizhenbo@yahoo.com>
2022-11-26 21:20:34 -0500

    Move test scripts

---
+++ fire_seq_search_server/debug_server.sh
@@ -0,0 +1,7 @@
+set -e
+cargo build
+rm ./fire_seq_search_server -f
+cp --force target/debug/fire_seq_search_server.exe ./fire_seq_search_server
+RUST_BACKTRACE=1 RUST_LOG=info ./fire_seq_search_server \
+--notebook_path /c/Users/z2369li/Nextcloud/logseq_notebook \
+--enable-journal-query
