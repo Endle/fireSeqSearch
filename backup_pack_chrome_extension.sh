fd14baab3eac4742178e55c7f08487db050ed04f
Zhenbo Li <3221521+Endle@users.noreply.github.com>
2023-01-20 13:00:39 -0500

    Delete backup_pack_chrome_extension.sh

--- backup_pack_chrome_extension.sh
+++
@@ -1,8 +0,0 @@
-rm -rf chrome_tmp
-cp -r fireSeqSearch_addon chrome_tmp
-cd chrome_tmp
-rm manifest.json
-mv manifest_chrome.json manifest.json
-zip -r -FS ../fireSeqSearch_chrome.zip * --exclude '*.git*'
-cd ..
-cp -f fireSeqSearch.zip /dev/shm
