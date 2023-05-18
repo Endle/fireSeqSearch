03d1e91eeed253c71b4801c4485a297a8261c4eb
Zhenbo Li <3221521+Endle@users.noreply.github.com>
2023-05-18 19:39:03 -0400

    Fix highlighting 'The' (#120)
    Compare the lowercase against stop word list

--- fire_seq_search_server/src/post_query/highlighter.rs
+++ fire_seq_search_server/src/post_query/highlighter.rs
@@ -18,10 +18,12 @@ pub fn highlight_keywords_in_body(body: &str, term_tokens: &Vec<String>,
     // let nltk = generate_stopwords_list();
     let nltk = &STOPWORDS_LIST;
 
-    let term_ref: Vec<&str> = term_tokens.iter().map(|s| &**s).collect();
-    let terms_selected: Vec<&str> = term_ref.into_iter()
-        .filter(|&s| !nltk.contains(s))
-        .collect();
+    let terms_selected: Vec<&str> = crate::language_tools::search_term::filter_out_stopwords(
+        &term_tokens, nltk);
+    // let term_ref: Vec<&str> = term_tokens.iter().map(|s| &**s).collect();
+    // let terms_selected: Vec<&str> = term_ref.into_iter()
+    //     .filter(|&s| !nltk.contains(s))
+    //     .collect();
     info!("Highlight terms: {:?}", &terms_selected);
 
 
@@ -224,8 +226,12 @@ pub fn locate_single_keyword<'a>(sentence: &'a str, token: &'a str) -> Vec<(usiz
 }
 
 
-
-fn generate_stopwords_list() -> std::collections::HashSet<String> {
+/// ```
+/// let l = fire_seq_search_server::post_query::highlighter::generate_stopwords_list();
+/// assert!(l.contains("the"));
+/// assert!(!l.contains("thex"));
+/// ```
+pub fn generate_stopwords_list() -> std::collections::HashSet<String> {
     use stopwords::Stopwords;
     let mut nltk: std::collections::HashSet<&str> = stopwords::NLTK::stopwords(stopwords::Language::English).unwrap().iter().cloned().collect();
     nltk.insert("span");
