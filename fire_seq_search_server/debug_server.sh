c5507493cb45be5856165fdd7161b40c2ba2d32b
Zhenbo Li <lizhenbo@yahoo.com>
2023-06-18 14:53:58 -0400

    Update debug_server.sh

--- fire_seq_search_server/debug_server.sh
+++ fire_seq_search_server/debug_server.sh
@@ -3,6 +3,5 @@ cargo build
 rm ./fire_seq_search_server -f
 cp --force target/debug/fire_seq_search_server.exe ./fire_seq_search_server
 RUST_BACKTRACE=1 RUST_LOG=debug ./fire_seq_search_server \
---notebook_path /c/Users/z2369li/Nextcloud/logseq_notebook \
---enable-journal-query
+--notebook_path /c/Users/z2369li/Documents/logseq_notebook \
 # --parse-pdf-links
