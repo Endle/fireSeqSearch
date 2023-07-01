ec286c84aba6e1635a7fe9c0ffc2ddcc540ce635
Zhenbo Li <lizhenbo@yahoo.com>
2023-07-01 19:40:42 -0400

    group by part finished naively

--- fire_seq_search_server/src/word_frequency/mod.rs
+++ fire_seq_search_server/src/word_frequency/mod.rs
@@ -1,14 +1,33 @@
 use log::info;
 use crate::Article;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
+
+use rayon::prelude::*;
+
+// let x: Vec<Vec<_>> = vec![vec![1, 2], vec![3, 4]];
+// let y: Vec<_> = x.into_par_iter().flatten().collect();
 pub fn generate_wordcloud(articles: &Vec<Article>) -> String {
     info!("Generating wordlist");
 
-    for art in articles {
-        let tokens = article_to_tokens(art);
+    let tokens: Vec<String> = articles.par_iter().map(article_to_tokens)
+        .flatten().collect();
+    // for art in articles {
+    //     let tokens = article_to_tokens(art);
+    // }
+    info!("After flatten, we got {} tokens", tokens.len());
+
+    // silly group by
+    let mut freq: HashMap<String,i64> = HashMap::new();
+    for t in tokens {
+        match freq.get(&t) {
+            Some(count) => { freq.insert(t, count + 1); }
+            None => { freq.insert(t, 1 as i64); }
+        }
     }
-    "stub_cloud".to_string()
+
+    let serialized_data = serde_json::to_string(&freq).unwrap();
+    serialized_data
 }
 
 fn article_to_tokens(art: &Article) -> Vec<String> {
