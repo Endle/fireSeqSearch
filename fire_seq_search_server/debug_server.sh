5ab9ccc5f244be979fa3ca1dcd03d240119ed044
Zhenbo Li <zhenboli@zhenbos-mini.home>
2023-10-13 22:45:17 -0400

    Edit local parameter

--- fire_seq_search_server/debug_server.sh
+++ fire_seq_search_server/debug_server.sh
@@ -1,8 +1,8 @@
 set -e
-cargo build
-rm ./fire_seq_search_server -f
-cp --force target/debug/fire_seq_search_server.exe ./fire_seq_search_server
+rm -f ./fire_seq_search_server 
+nix-shell -p cargo -p rustc -p libiconv --run "cargo build"
+cp  target/debug/fire_seq_search_server ./fire_seq_search_server
 RUST_BACKTRACE=1 RUST_LOG=debug ./fire_seq_search_server \
---notebook_path /c/Users/z2369li/Documents/logseq_notebook \
+--notebook_path /Users/zhenboli/logseq \
 --exclude-zotero-items
 # --parse-pdf-links
