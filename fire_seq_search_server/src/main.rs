03daaa7f336761ad497cde4e4a64e462c8c69ef9
Zhenbo Li <lizhenbo@yahoo.com>
2024-09-01 13:22:22 -0400

    avoid pushing too much info to server at rust side

--- fire_seq_search_server/src/main.rs
+++ fire_seq_search_server/src/main.rs
@@ -78,12 +78,10 @@ async fn main() {
         engine.llm = Some(llm_arc);
 
         let poll_handle = tokio::spawn( async move {
-            info!("inside main loop");
             loop {
                 llm_poll.call_llm_engine().await;
                 let wait_llm = tokio::time::Duration::from_millis(500);
                 tokio::time::sleep(wait_llm).await;
-                info!("main loop: poll again");
             }
         });
 //        poll_handle.await;
