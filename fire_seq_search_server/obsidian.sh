b7741e8aeb5796f9722d7dd1f4ab212be8a3a870
Zhenbo Li <lizhenbo@yahoo.com>
2024-09-21 15:07:04 -0400

    create doc base file

--- fire_seq_search_server/obsidian.sh
+++ fire_seq_search_server/obsidian.sh
@@ -3,6 +3,8 @@ cargo build --features llm
 rm ./fire_seq_search_server -f
 cp --force target/debug/fire_seq_search_server ./fire_seq_search_server
 
+NOTEBOOK_NAME=AstroWiki_2.0-main
+
 RUST_BACKTRACE=1 RUST_LOG=debug ./fire_seq_search_server \
-    --notebook_path ~/Documents/obsidian-hub-main \
+    --notebook_path ~/Documents/$NOTEBOOK_NAME \
     --obsidian-md
